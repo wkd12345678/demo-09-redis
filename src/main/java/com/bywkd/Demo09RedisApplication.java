f56040ecc4be56397e6fddd3937a17d85839fe6c
wkd <773109065@qq.com>
2022-03-11 16:38:18 +0800

    git@github.com:wkd12345678/git-one.git

---
+++ src/main/java/com/bywkd/Demo09RedisApplication.java
@@ -0,0 +1,13 @@
+package com.bywkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo09RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo09RedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bywkd/Demo09RedisApplicationTests.java
@@ -0,0 +1,21 @@
+package com.bywkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class Demo09RedisApplicationTests {
+
+    @Autowired
+    RedisTemplate redisTemplate;
+    @Test
+    void contextLoads() {
+        //没有redis服务器会报错
+        redisTemplate.opsForValue().set("ss","ee");
+        System.out.println(redisTemplate.opsForValue().get("ss"));
+
+    }
+
+}
