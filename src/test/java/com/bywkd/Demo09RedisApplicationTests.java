cce8669fe92dada442095dc17aa860da9d9fef4e
wkd <773109065@qq.com>
2022-03-11 17:44:22 +0800

    ddd3

--- src/test/java/com/bywkd/Demo09RedisApplicationTests.java
+++ src/test/java/com/bywkd/Demo09RedisApplicationTests.java
@@ -16,6 +16,7 @@ class Demo09RedisApplicationTests {
         redisTemplate.opsForValue().set("ss","ee");
         System.out.println(redisTemplate.opsForValue().get("ss"));
         System.out.println("two");
+        System.out.println("sss");
 
     }
 
